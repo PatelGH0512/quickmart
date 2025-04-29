c0b386385d51e679a4c48594e27f90503fffcea3
pateldevarshi <pateldevarshi111@gmail.comclear>
2025-04-29 02:44:59 -0400

    speed insights 1

--- Desktop/Projects/greencart/client/src/main.jsx
+++ Desktop/Projects/greencart/client/src/main.jsx
@@ -3,13 +3,11 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AppContextProvider } from "./context/AppContext.jsx";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AppContextProvider>
       <App />
-      <SpeedInsights></SpeedInsights>
     </AppContextProvider>
   </BrowserRouter>
 );
